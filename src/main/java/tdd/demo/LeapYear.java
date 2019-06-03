7dc79ad5ddd7c54623706d57a4290be9f38ab8ea
金正朋也 <mhtnim1023@gmail.com>
2019-06-03 21:34:48 +0900

    閏年作成

--- src/main/java/tdd/demo/LeapYear.java
+++ src/main/java/tdd/demo/LeapYear.java
@@ -1,5 +1,22 @@
 package tdd.demo;
 
 public class LeapYear {
-    
+
+    public static boolean isLeep(int year) {
+        boolean result = false;
+        if(year % 4 != 0) {
+            result = false;
+        } else {
+            if(year % 100 != 0) {
+                result = true;
+            } else {
+                if(year % 400 != 0) {
+                    result = false;
+                } else {
+                    result = true;
+                }
+            }
+        }
+        return result;
+    }
 }
--- src/test/java/tdd/demo/LeepYearTest.java
+++ src/test/java/tdd/demo/LeepYearTest.java
@@ -7,8 +7,23 @@ import static org.junit.Assert.assertThat;
 
 public class LeepYearTest {
     @Test
-    public void _年歴が4で割り切れない場合にfalseを返す() {
+    public void _年歴が2001の時にfalseを返す() {
         LeapYear leapYear;
-        assertThat(LeapYear.isLeep(), is(false));
+        assertThat(LeapYear.isLeep(2001), is(false));
+    }
+
+    @Test
+    public void _年歴が2004の時にtrueを返す() {
+        assertThat(LeapYear.isLeep(2004), is(true));
+    }
+
+    @Test
+    public void _年歴が2000の時にtrueを返す() {
+        assertThat(LeapYear.isLeep(2000), is(true));
+    }
+
+    @Test
+    public void _年歴が1900の時にfalseを返す() {
+        assertThat(LeapYear.isLeep(1900), is(false));
     }
 }
